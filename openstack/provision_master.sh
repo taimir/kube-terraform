2b43fb2ae0e64a49441cb24611f3cfb8045cbac6
Atanas Mirchev <atanas.mirchev@est.fujitsu.com>
2016-09-22 14:00:44 +0200

    Update master provisioning script.

--- openstack/provision_master.sh
+++ openstack/provision_master.sh
@@ -11,9 +11,9 @@ curl -sSL https://get.docker.com/ | sh
 sudo apt-get install -y socat
 curl -s -L "https://www.dropbox.com/s/shhs46bzhex7dxo/debs-9b4337.txz?dl=1" | tar xJv
 sudo dpkg -i debian/bin/*.deb
-systemctl daemon-reload && systemctl restart kubelet
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
 
 
 # Bootstrap the k8s master
-kubeadm init
+sudo kubeadm init
 
